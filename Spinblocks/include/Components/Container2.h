b846a693999328bdcd036d8e6eddb780a76a3920
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-02-24 19:14:13 -0800

    Some grid dimension and cell position calculations in Container2.

--- Spinblocks/include/Components/Container2.h
+++ Spinblocks/include/Components/Container2.h
@@ -8,15 +8,15 @@ namespace Components
 	{
 	protected:
 		glm::uvec2 m_gridDimensions; // In grid coordinates, not screen position.
-		glm::uvec2 m_cellDimensions; // In screen coordinates
+		glm::vec2 m_cellDimensions; // In screen coordinates
 	
 	public:
-		Container2(const glm::uvec2& gridDimensions, const glm::uvec2& cellDimensions) : m_gridDimensions(gridDimensions), m_cellDimensions(cellDimensions)
+		Container2(const glm::uvec2& gridDimensions, const glm::vec2& cellDimensions) : m_gridDimensions(gridDimensions), m_cellDimensions(cellDimensions)
 		{
 
 		}
 
-		const glm::vec2& GetGridDimensions() const
+		const glm::uvec2& GetGridDimensions() const
 		{
 			return m_gridDimensions;
 		}
@@ -24,6 +24,32 @@ namespace Components
 		const glm::vec2& GetCellDimensions() const
 		{
 			return m_cellDimensions;
+		}
+
+		const glm::vec3 GetCellDimensions3() const
+		{
+			glm::vec2 dim = GetCellDimensions();
+			return glm::vec3(dim.x, dim.y, 1.0f);
+		}
+
+		const glm::vec2 GetCellPosition(const glm::vec3& parentPosition, const glm::uvec2& coordinates) const
+		{
+			int x = 0;
+			int y = 0;
+
+			x = -(m_gridDimensions.x / 2 * m_cellDimensions.x);
+			x += coordinates.x * m_cellDimensions.x;
+
+			y = -(m_gridDimensions.y / 2 * m_cellDimensions.y);
+			y += coordinates.y * m_cellDimensions.y;
+
+			return glm::vec2(parentPosition.x + x, parentPosition.y + y);
+		}
+
+		const glm::vec3 GetCellPosition3(const glm::vec3& parentPosition, const glm::uvec2& coordinates) const
+		{
+			glm::vec2 pos = GetCellPosition(parentPosition, coordinates);
+			return glm::vec3(pos.x, pos.y, parentPosition.z);
 		}
 	};
 
