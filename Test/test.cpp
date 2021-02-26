aaf3789a822b46c7e51314401b88d25c71efbaf0
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-02-26 13:01:38 -0800

    More test cases for the grid construction.

--- Test/test.cpp
+++ Test/test.cpp
@@ -200,3 +200,220 @@ TEST(GridTest, Grid1x2) {
 		}
 	}
 }
+
+TEST(GridTest, Grid2x2) {
+	entt::registry registry;
+
+	const auto playArea = registry.create();
+	registry.emplace<Components::Position>(playArea, glm::vec3(displayData.x / 2, displayData.y / 2, 0.0f));
+	registry.emplace<Components::Scale>(playArea);
+	registry.emplace<Components::Container2>(playArea, glm::uvec2(2, 2), glm::vec2(25, 25));
+	registry.emplace<Components::Tag>(playArea, "Play Area");
+
+
+	BuildGrid(registry, playArea);
+
+	auto gridCellView = registry.view<Components::Cell, Components::Coordinate, Components::Tag, Components::Scale, Components::Position>();
+	for (auto entity : gridCellView)
+	{
+		auto& cell = gridCellView.get<Components::Cell>(entity);
+		auto& coordinate = gridCellView.get<Components::Coordinate>(entity);
+
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+	}
+}
+
+TEST(GridTest, Grid3x1) {
+	entt::registry registry;
+
+	const auto playArea = registry.create();
+	registry.emplace<Components::Position>(playArea, glm::vec3(displayData.x / 2, displayData.y / 2, 0.0f));
+	registry.emplace<Components::Scale>(playArea);
+	registry.emplace<Components::Container2>(playArea, glm::uvec2(3, 1), glm::vec2(25, 25));
+	registry.emplace<Components::Tag>(playArea, "Play Area");
+
+
+	BuildGrid(registry, playArea);
+
+	auto gridCellView = registry.view<Components::Cell, Components::Coordinate, Components::Tag, Components::Scale, Components::Position>();
+	for (auto entity : gridCellView)
+	{
+		auto& cell = gridCellView.get<Components::Cell>(entity);
+		auto& coordinate = gridCellView.get<Components::Coordinate>(entity);
+
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 2 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+	}
+}
+
+TEST(GridTest, Grid1x3) {
+	entt::registry registry;
+
+	const auto playArea = registry.create();
+	registry.emplace<Components::Position>(playArea, glm::vec3(displayData.x / 2, displayData.y / 2, 0.0f));
+	registry.emplace<Components::Scale>(playArea);
+	registry.emplace<Components::Container2>(playArea, glm::uvec2(1, 3), glm::vec2(25, 25));
+	registry.emplace<Components::Tag>(playArea, "Play Area");
+
+
+	BuildGrid(registry, playArea);
+
+	auto gridCellView = registry.view<Components::Cell, Components::Coordinate, Components::Tag, Components::Scale, Components::Position>();
+	for (auto entity : gridCellView)
+	{
+		auto& cell = gridCellView.get<Components::Cell>(entity);
+		auto& coordinate = gridCellView.get<Components::Coordinate>(entity);
+
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 2)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+	}
+}
+
+TEST(GridTest, Grid3x3) {
+	entt::registry registry;
+
+	const auto playArea = registry.create();
+	registry.emplace<Components::Position>(playArea, glm::vec3(displayData.x / 2, displayData.y / 2, 0.0f));
+	registry.emplace<Components::Scale>(playArea);
+	registry.emplace<Components::Container2>(playArea, glm::uvec2(3, 3), glm::vec2(25, 25));
+	registry.emplace<Components::Tag>(playArea, "Play Area");
+
+
+	BuildGrid(registry, playArea);
+
+	auto gridCellView = registry.view<Components::Cell, Components::Coordinate, Components::Tag, Components::Scale, Components::Position>();
+	for (auto entity : gridCellView)
+	{
+		auto& cell = gridCellView.get<Components::Cell>(entity);
+		auto& coordinate = gridCellView.get<Components::Coordinate>(entity);
+
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 2 && coordinate.Get().y == 0)
+		{
+			EXPECT_TRUE(cell.GetNorth() == entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 2 && coordinate.Get().y == 1)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() != entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 0 && coordinate.Get().y == 2)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() == entt::null);
+		}
+		if (coordinate.Get().x == 1 && coordinate.Get().y == 2)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() != entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+		if (coordinate.Get().x == 2 && coordinate.Get().y == 2)
+		{
+			EXPECT_TRUE(cell.GetNorth() != entt::null);
+			EXPECT_TRUE(cell.GetEast() == entt::null);
+			EXPECT_TRUE(cell.GetSouth() == entt::null);
+			EXPECT_TRUE(cell.GetWest() != entt::null);
+		}
+	}
+}
