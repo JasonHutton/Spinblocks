2d4136a9c43ecb97bb7be6fc502a86ce72b22bd3
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-03-16 15:33:04 -0700

    WIP of detaching unsupported pieces when a line has been cleared.

--- Spinblocks/src/Systems/EliminateSystem.cpp
+++ Spinblocks/src/Systems/EliminateSystem.cpp
@@ -2,15 +2,39 @@
 #include "Systems/SystemShared.h"
 #include "Utility.h"
 
+#include <set>
+
 namespace Systems
 {
 	void EliminateSystem(entt::registry& registry, double currentFrameTime)
 	{
+		auto rows = std::set<unsigned int>();
+
 		// Clear all hitlist marked ents
 		auto hittableView = registry.view<Components::Block, Components::Hittable>();
 		for (auto entity : hittableView)
 		{
+			rows.insert(GetCoordinateOfEntity(registry, entity).Get().y); // Note all unique rows that have been cleared.
+
 			registry.destroy(entity);
+		}
+
+
+		if (rows.size() > 0)
+		{
+			unsigned int lowestRow = *rows.begin();
+
+			auto blockView = registry.view<Components::Block, Components::Moveable>();
+			for (auto entity : blockView)
+			{
+				auto& moveable = blockView.get<Components::Moveable>(entity);
+
+				if (moveable.GetCurrentCoordinate().Get().y > lowestRow)
+				{
+					moveable.SetDesiredCoordinate(GetCoordinateOfEntity(registry, MoveBlockInDirection(registry, "Play Area", entity, moveDirection_t::SOUTH, rows.size())));
+					moveable.SetMovementState(Components::movementStates_t::HARD_DROP);
+				}
+			}
 		}
 	}
 }
