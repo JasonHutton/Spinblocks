e7b25ad9bb2c59bb347257d4865f4612a9aafe42
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-03-17 07:02:50 -0700

    Add GetCellAtCoordinates2 declaration to #Utility.h

--- Spinblocks/include/Utility.h
+++ Spinblocks/include/Utility.h
@@ -27,5 +27,6 @@ bool CanOccupyCell(entt::registry& registry, const std::string& containerTag, co
 const Components::Coordinate& GetCoordinateOfEntity(entt::registry& registry, const entt::entity& entity);
 const Components::Cell& GetCellOfEntity(entt::registry& registry, const entt::entity& entity);
 Components::Cell& GetCellAtCoordinates(entt::registry& registry, const std::string& containerTag, const Components::Coordinate& coordinate);
+const entt::entity& GetCellAtCoordinates2(entt::registry& registry, const std::string& containerTag, const Components::Coordinate& coordinate);
 const Components::Block& GetBlockAtCoordinates(entt::registry& registry, const std::string& containerTag, const Components::Coordinate& coordinate);
 entt::entity MoveBlockInDirection(entt::registry& registry, const std::string& containerTag, const entt::entity& blockEnt, const moveDirection_t& direction, const unsigned int& distance, const bool& disableObstruction = false);
