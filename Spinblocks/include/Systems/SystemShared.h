d8863c1393b4264b0d723e0f31eb2dc74dacba6f
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-03-20 18:02:52 -0700

    Add BufferAreaHeight global variable.

--- Spinblocks/include/Systems/SystemShared.h
+++ Spinblocks/include/Systems/SystemShared.h
@@ -4,6 +4,7 @@
 //{
 	const unsigned int PlayAreaWidth = 10; // This shouldn't be done this way, but for now this is okay. FIXME TODO
 	const unsigned int PlayAreaHeight = 20; // This shouldn't be done this way, but for now this is okay. FIXME TODO
+	const unsigned int BufferAreaHeight = 20; // This shouldn't be done this way, but for now this is okay. FIXME TODO
 	const double KeyRepeatDelay = 0.3; // Delay before starting to repeat.
 	const double KeyRepeatRate = 0.5 / PlayAreaWidth; // Delay between repeats.
 	const double FallSpeed = 1.0; // Base fall speed, time it takes to move 1 line.
