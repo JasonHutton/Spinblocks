d5b415bd882892f533beb97002403584cc7051c3
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-03 13:07:34 -0700

    Some checks in StateChangeSystem() to be more general instead of block-specific, and handle tetrominos a bit. This is a bit messy, but mostly works. Also kill tetrominos once they've locked down, leaving their blocks behind.

--- Spinblocks/src/Systems/StateChangeSystem.cpp
+++ Spinblocks/src/Systems/StateChangeSystem.cpp
@@ -6,16 +6,27 @@ namespace Systems
 {
 	void StateChangeSystem(entt::registry& registry, double currentFrameTime)
 	{
-		auto blockView = registry.view<Components::Block>();
+		/*auto tetrominoView = registry.view<Components::Moveable, Components::Coordinate>(entt::exclude<Components::Obstructable>);
+		for (auto entity : tetrominoView)
+		{
+			if (!IsEntityTetromino(registry, entity)) // There should be a better way to do this. Look into it later. FIXME TODO
+				continue;
+
+			auto& moveable = blockView.get<Components::Moveable>(entity);
+			auto& coordinate = blockView.get<Components::Coordinate>(entity);
+
+
+		}*/
+
+		auto blockView = registry.view<Components::Obstructable, Components::Moveable, Components::Coordinate>();
 		for (auto entity : blockView)
 		{
-			auto& block = blockView.get<Components::Block>(entity);
-			auto& moveable = registry.get<Components::Moveable>(entity);
-			auto& coordinate = registry.get<Components::Coordinate>(entity);
-			auto& obstructable = registry.get<Components::Obstructable>(entity);
-			auto& obstructs = registry.get<Components::Obstructs>(entity);
+			//auto& block = registry.get<Components::Block>(entity);
+			auto& moveable = blockView.get<Components::Moveable>(entity);
+			auto& coordinate = blockView.get<Components::Coordinate>(entity);
+			auto& obstructable = blockView.get<Components::Obstructable>(entity);
 
-			if (block.IsEnabled() && moveable.IsEnabled() && coordinate.IsEnabled() && obstructable.IsEnabled())
+			if (moveable.IsEnabled() && coordinate.IsEnabled() && obstructable.IsEnabled())
 			{
 				switch (moveable.GetMovementState())
 				{
@@ -25,14 +36,34 @@ namespace Systems
 						moveable.SetMovementState(Components::movementStates_t::LOCKED);
 						registry.remove_if_exists<Components::Controllable>(entity);
 					}
+
+					if (IsEntityTetromino(registry, entity))
+					{
+						auto* tetromino = GetTetrominoFromEntity(registry, entity);
+
+						if (tetromino->GetAreAllBlocksObstructed(registry) && currentFrameTime >= tetromino->GetAllBlocksLastObstructedTime(registry) + lockdownDelay)
+						{
+							tetromino->SetAllBlocksMovementState(registry, Components::movementStates_t::LOCKED);
+						}
+					}
+					
 					// Do nothing
 					//lastFallUpdate = currentFrameTime; // Reset the fall time, to avoid a change of state here resulting in an immediate fall, which manifests as a double-move, which feels bad.
 					//moveable.SetMovementState(Components::movementStates_t::FALL);
 					break;
 				case Components::movementStates_t::DEBUG_MOVE_UP:
 					lastFallUpdate = currentFrameTime; // Reset the fall time, to avoid a change of state here resulting in an immediate fall, which manifests as a double-move, which feels bad.
+
 					obstructable.SetIsObstructed(false);
 					moveable.SetMovementState(Components::movementStates_t::FALL); // Reset to falling state for the next tick.
+					if (IsEntityTetromino(registry, entity))
+					{
+						auto* tetromino = GetTetrominoFromEntity(registry, entity);
+						tetromino->SetAllBlocksObstructed(registry, false);
+						//tetromino->SetAllBlocksMovementState(registry, Components::movementStates_t::FALL);
+					}
+					
+					
 					break;
 				case Components::movementStates_t::SOFT_DROP:
 					if (obstructable.GetIsObstructed())
@@ -40,6 +71,16 @@ namespace Systems
 						// This never gets called, probably due to the fall state being set places instead, and the obstructed flag being set from the fall state. Not necessarily a problem.
 						moveable.SetMovementState(Components::movementStates_t::LOCKED);
 						registry.remove_if_exists<Components::Controllable>(entity);
+
+						if (IsEntityTetromino(registry, entity))
+						{
+							auto* tetromino = GetTetrominoFromEntity(registry, entity);
+
+							if (tetromino->GetAreAllBlocksObstructed(registry))
+							{
+								tetromino->SetAllBlocksMovementState(registry, Components::movementStates_t::LOCKED);
+							}
+						}
 					}
 					else
 					{
@@ -52,10 +93,38 @@ namespace Systems
 					lastFallUpdate = currentFrameTime; // Reset the fall time, to avoid a change of state here resulting in an immediate fall, which manifests as a double-move, which feels bad.
 					moveable.SetMovementState(Components::movementStates_t::LOCKED);
 					registry.remove_if_exists<Components::Controllable>(entity);
+					if (IsEntityTetromino(registry, entity))
+					{
+						auto* tetromino = GetTetrominoFromEntity(registry, entity);
+
+						if (tetromino->GetAreAllBlocksObstructed(registry))
+						{
+							tetromino->SetAllBlocksMovementState(registry, Components::movementStates_t::LOCKED);
+						}
+					}
 					break;
 				default:
 					break;
 				}
+			}
+		}
+
+		// Kill tetrominoes, leaving their blocks behind, after they're locked down.
+		auto tetrominoView = registry.view<Components::Moveable, Components::Coordinate>(entt::exclude<Components::Obstructs>);
+		for (auto entity : tetrominoView)
+		{
+			if (!IsEntityTetromino(registry, entity)) // There should be a better way to do this. Look into it later. FIXME TODO
+				continue;
+
+			auto* tetromino = GetTetrominoFromEntity(registry, entity);
+			if (tetromino->GetAreAllBlocksLocked(registry))
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					registry.remove_if_exists<Components::Follower>(tetromino->GetBlock(i));
+				}
+
+				registry.destroy(entity);
 			}
 		}
 	}
