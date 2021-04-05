09de92799577dbf890938dac1dfa79d87ad606b6
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-04 22:33:37 -0700

    Add more block patterns for the I Tetromino.

--- Spinblocks/include/Components/Tetrominos/ITetromino.h
+++ Spinblocks/include/Components/Tetrominos/ITetromino.h
@@ -27,8 +27,20 @@ namespace Components
 		void DefineBlockPattern() override
 		{
 			AddBlockPattern(0, moveDirection_t::NORTH, rotationDirection_t::NONE, { glm::vec2(0,2), glm::vec2(1,2), glm::vec2(2,2), glm::vec2(3,2) });
-			AddBlockPattern(0, moveDirection_t::NORTH, rotationDirection_t::CLOCKWISE, { glm::vec2(2,0), glm::vec2(2,1), glm::vec2(2,2), glm::vec2(2,3) });
-			AddBlockPattern(0, moveDirection_t::NORTH, rotationDirection_t::COUNTERCLOCKWISE, { glm::vec2(1,0), glm::vec2(1,1), glm::vec2(1,2), glm::vec2(1,3) });
+			AddBlockPattern(0, moveDirection_t::NORTH, rotationDirection_t::CLOCKWISE, std::make_tuple(0, moveDirection_t::EAST, rotationDirection_t::NONE));
+			AddBlockPattern(0, moveDirection_t::NORTH, rotationDirection_t::COUNTERCLOCKWISE, std::make_tuple(0, moveDirection_t::WEST, rotationDirection_t::NONE));
+
+			AddBlockPattern(0, moveDirection_t::EAST, rotationDirection_t::NONE, { glm::vec2(2,0), glm::vec2(2,1), glm::vec2(2,2), glm::vec2(2,3) });
+			AddBlockPattern(0, moveDirection_t::EAST, rotationDirection_t::CLOCKWISE, std::make_tuple(0, moveDirection_t::SOUTH, rotationDirection_t::NONE));
+			AddBlockPattern(0, moveDirection_t::EAST, rotationDirection_t::COUNTERCLOCKWISE, std::make_tuple(0, moveDirection_t::NORTH, rotationDirection_t::NONE));
+
+			AddBlockPattern(0, moveDirection_t::SOUTH, rotationDirection_t::NONE, { glm::vec2(0,1), glm::vec2(1,1), glm::vec2(2,1), glm::vec2(3,1) });
+			AddBlockPattern(0, moveDirection_t::SOUTH, rotationDirection_t::CLOCKWISE, std::make_tuple(0, moveDirection_t::WEST, rotationDirection_t::NONE));
+			AddBlockPattern(0, moveDirection_t::SOUTH, rotationDirection_t::COUNTERCLOCKWISE, std::make_tuple(0, moveDirection_t::EAST, rotationDirection_t::NONE));
+
+			AddBlockPattern(0, moveDirection_t::WEST, rotationDirection_t::NONE, { glm::vec2(1,0), glm::vec2(1,1), glm::vec2(1,2), glm::vec2(1,3) });
+			AddBlockPattern(0, moveDirection_t::WEST, rotationDirection_t::CLOCKWISE, std::make_tuple(0, moveDirection_t::NORTH, rotationDirection_t::NONE));
+			AddBlockPattern(0, moveDirection_t::WEST, rotationDirection_t::CLOCKWISE, std::make_tuple(0, moveDirection_t::SOUTH, rotationDirection_t::NONE));
 		}
 
 		void DefineRotationPoints() override
