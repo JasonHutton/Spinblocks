d27a846c9774673c7d2dc685dc77553e1814fe0b
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-08 16:58:01 -0700

    Comment out debugging Wall in middle of playfield.

--- Spinblocks/src/Main.cpp
+++ Spinblocks/src/Main.cpp
@@ -911,12 +911,12 @@ int main()
 	BuildGrid(registry, bagArea);
 
 	//PlaceMarker(registry, GetTagFromContainerType(containerType_t::MATRIX), "Matrix Edge 1", Components::Coordinate(matrix, glm::uvec2(0, 0)));
-	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 0)));
+	/*PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 0)));
 	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 1)));
 	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 2)));
 	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 3)));
 	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 4)));
-	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 5)));
+	PlaceWall(registry, Components::Coordinate(matrix, glm::uvec2(5, 5)));*/
 
 	/*
 	Components::Container2 container2 = registry.get<Components::Container2>(playArea);
