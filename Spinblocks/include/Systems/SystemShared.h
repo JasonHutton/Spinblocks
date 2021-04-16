0b63c9798cda06391208362ca65c57ba50d3e7f8
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-16 03:51:31 -0700

    Switch to using signed vs unsigned constants, as we need to manipulate them some.

--- Spinblocks/include/Systems/SystemShared.h
+++ Spinblocks/include/Systems/SystemShared.h
@@ -2,9 +2,9 @@
 
 //namespace Systems
 //{
-	const unsigned int PlayAreaWidth = 10; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Width of the play area when north facing
-	const unsigned int PlayAreaHeight = 20; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Height of the play area when north facing
-	const unsigned int BufferAreaDepth = 4; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Depth of the buffer area around the play area, on all sides.
+	const int PlayAreaWidth = 10; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Width of the play area when north facing
+	const int PlayAreaHeight = 20; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Height of the play area when north facing
+	const int BufferAreaDepth = 5; // This shouldn't be done this way, but for now this is okay. FIXME TODO // Depth of the buffer area around the play area, on all sides.
 	const double KeyRepeatDelay = 0.3; // Delay before starting to repeat.
 	const double KeyRepeatRate = 0.5 / PlayAreaWidth; // Delay between repeats. // This does not change when changing orientation.
 	const double FallSpeed = 1.0; // Base fall speed, time it takes to move 1 line.
