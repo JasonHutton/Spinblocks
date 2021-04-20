6548488a124765a15900171642d7aab58092557f
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-20 04:50:54 -0700

    More fluff.

--- Spinblocks/src/Systems/BoardRotateSystem.cpp
+++ Spinblocks/src/Systems/BoardRotateSystem.cpp
@@ -6,6 +6,8 @@ namespace Systems
 {
 	rotationDirection_t BoardRotateSystem(entt::registry& registry, double currentFrameTime, rotationDirection_t rotationDirection)
 	{
-		return rotationDirection_t::NONE;
+		RotatePlayArea(registry, rotationDirection);
+
+		return rotationDirection;
 	}
 }
