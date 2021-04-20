1fe85fb9499492d03bfbf7ef8c43b4976ab330d8
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-19 17:37:15 -0700

    Refactor GenerationSystem to be easier to read. Still has issues.

--- Spinblocks/src/Systems/GenerationSystem.cpp
+++ Spinblocks/src/Systems/GenerationSystem.cpp
@@ -8,6 +8,96 @@
 
 namespace Systems
 {
+	void FillNodeQueue(entt::registry& registry, double currentFrameTime, entt::entity nodeEnt)
+	{
+		if (!registry.all_of<Components::QueueNode>(nodeEnt))
+			return;
+
+		auto& node = registry.get<Components::QueueNode>(nodeEnt);
+
+		// Node is not empty.
+		if (node.GetContent() != entt::null)
+		{
+			if (registry.all_of<Components::QueueNode>(node.GetDestination()))
+			{
+				auto& destination = registry.get<Components::QueueNode>(node.GetDestination());
+				if (destination.GetContent() == entt::null)
+				{ // Destination is empty
+					auto& nodeCoordinates = registry.get<Components::Coordinate>(node.GetDestination());
+					destination.SetContent(node.GetContent());
+					node.SetContent(entt::null);
+					RelocateTetromino(registry, nodeCoordinates, destination.GetContent());
+				}
+			}
+		}
+		else // Node is empty
+		{
+			if (registry.all_of<Components::QueueNode>(node.GetSource()))
+			{ // Source is a node.
+				// Look at that node.
+				FillNodeQueue(registry, currentFrameTime, node.GetSource());
+			}
+			else if (registry.all_of<Components::Bag>(node.GetSource()))
+			{ // Source is not a node. It's a bag component.
+				// Spawn a new Tetromino here, I think.
+				auto& bag = registry.get<Components::Bag>(node.GetSource());
+				auto& nodeCoordinates = registry.get<Components::Coordinate>(nodeEnt);
+				node.SetContent(SpawnTetromino(registry, GetTagFromContainerType(containerType_t::BAG_AREA), nodeCoordinates, bag.PopTetromino(), false));
+			}
+		}
+	}
+
+	void PopFromQueueIntoMatrix(entt::registry& registry, double currentFrameTime, entt::entity nodeEnt)
+	{
+		if (!registry.all_of<Components::QueueNode>(nodeEnt))
+			return;
+
+		auto& node = registry.get<Components::QueueNode>(nodeEnt);
+
+		entt::entity tet = node.GetContent();
+		node.SetContent(entt::null);
+
+		auto* tetromino = GetTetrominoFromEntity(registry, tet);
+		if (tetromino != NULL)
+		{
+			auto newCoordinate = Components::Coordinate(node.GetDestination(), GetTetrominoSpawnCoordinates(registry, GetTagFromContainerType(containerType_t::MATRIX), tetromino->GetType()));
+
+			RelocateTetromino(registry, newCoordinate, tet);
+
+			registry.emplace<Components::Controllable>(tet, newCoordinate.GetParent());
+			if (registry.all_of<Components::Moveable>(tet))
+			{
+				auto& moveable = registry.get<Components::Moveable>(tet);
+				moveable.SetMovementState(Components::movementStates_t::FALL);
+
+				for (int i = 0; i < 4; i++)
+				{
+					auto& blockMoveable = registry.get<Components::Moveable>(tetromino->GetBlock(i));
+					if (registry.all_of<Components::Follower>(tetromino->GetBlock(i)))
+					{
+						blockMoveable.SetMovementState(Components::movementStates_t::FOLLOWING);
+					}
+					else
+					{
+						blockMoveable.SetMovementState(Components::movementStates_t::FALL);
+					}
+				}
+			}
+
+			if (IsAnyBlockInTetrominoObstructed(registry, tet))
+			{
+				cout << "Obstructed on spawn! Game Over!" << endl;
+
+				auto controllableView = registry.view<Components::Controllable>();
+				for (auto controllable : controllableView)
+				{
+					registry.remove_if_exists<Components::Controllable>(controllable);
+				}
+				GameState::SetState(gameState_t::GAME_OVER);
+			}
+		}
+	}
+
 	void GenerationSystem(entt::registry& registry, double currentFrameTime)
 	{
 		const auto& bagAreaEnt = FindEntityByTag(registry, GetTagFromContainerType(containerType_t::BAG_AREA));
@@ -21,99 +111,47 @@ namespace Systems
 		auto& nodeOrder = registry.get<Components::NodeOrder>(bagAreaEnt);
 		entt::entity nodeEnt = entt::null;
 
-		if (currentFrameTime >= lastLockdownTime + generationTimeDelay && registry.view<Components::Controllable>().size() == 0)
+		// Fill the queue if it's not full
+		bool atLeastOneNodeIsEmpty = false;
+		do
 		{
-			entt::entity lastNode = nodeEnt;
-			bool allSlotsFull = true;
-			do
+			atLeastOneNodeIsEmpty = false;
+			auto nodeView = registry.view<Components::QueueNode>();
+			for (auto entity : nodeView)
 			{
-				if (nodeEnt != entt::null)
-					lastNode = nodeEnt;
+				auto& node = registry.get<Components::QueueNode>(entity);
+				if (node.GetContent() == entt::null)
+				{
+					atLeastOneNodeIsEmpty = true;
+				}
+			}
+
+			if (atLeastOneNodeIsEmpty)
+			{
 				nodeEnt = nodeOrder.GetNode();
 				if (nodeEnt != entt::null)
 				{
 					auto& node = registry.get<Components::QueueNode>(nodeEnt);
+					// First node is the source node. Bag area entity 3
+
+					// This node is empty.
 					if (node.GetContent() == entt::null)
-						allSlotsFull = false;
-				}
-			} while (nodeEnt != entt::null);
-
-			if (allSlotsFull)
-			{
-				auto& node = registry.get<Components::QueueNode>(lastNode);
-				if (node.GetContent() != entt::null && node.GetDestination() == matrixEnt)
-				{
-					auto* tetromino = GetTetrominoFromEntity(registry, node.GetContent());
-					if (tetromino != NULL)
 					{
-						entt::entity tet = node.GetContent();
-						auto newCoordinate = Components::Coordinate(matrixEnt, GetTetrominoSpawnCoordinates(registry, GetTagFromContainerType(containerType_t::MATRIX), tetromino->GetType()));
-
-						RelocateTetromino(registry, newCoordinate, tet);
-
-						registry.emplace<Components::Controllable>(tet, newCoordinate.GetParent());
-						if (registry.all_of<Components::Moveable>(tet))
-						{
-							auto& moveable = registry.get<Components::Moveable>(tet);
-							moveable.SetMovementState(Components::movementStates_t::FALL);
-
-							for (int i = 0; i < 4; i++)
-							{
-								auto& blockMoveable = registry.get<Components::Moveable>(tetromino->GetBlock(i));
-								if (registry.all_of<Components::Follower>(tetromino->GetBlock(i)))
-								{
-									blockMoveable.SetMovementState(Components::movementStates_t::FOLLOWING);
-								}
-								else
-								{
-									blockMoveable.SetMovementState(Components::movementStates_t::FALL);
-								}
-							}
-						}
-
-						if (IsAnyBlockInTetrominoObstructed(registry, tet))
-						{
-							cout << "Obstructed on spawn! Game Over!" << endl;
-
-							auto controllableView = registry.view<Components::Controllable>();
-							for (auto controllable : controllableView)
-							{
-								registry.remove_if_exists<Components::Controllable>(controllable);
-							}
-							GameState::SetState(gameState_t::GAME_OVER);
-						}
+						FillNodeQueue(registry, currentFrameTime, nodeEnt);
 					}
-
-					node.SetContent(entt::null);
 				}
 			}
-			else
-			{
-				nodeEnt = entt::null;
-				while ((nodeEnt = nodeOrder.GetNode()) != entt::null)
-				{
-					auto& node = registry.get<Components::QueueNode>(nodeEnt);
-					auto& nodeCoordinates = registry.get<Components::Coordinate>(nodeEnt);
-					if (node.GetContent() == entt::null)
-					{
-						if (registry.all_of<Components::Bag>(node.GetSource()))
-						{
-							auto& bag = registry.get<Components::Bag>(node.GetSource());
-							node.SetContent(SpawnTetromino(registry, GetTagFromContainerType(containerType_t::BAG_AREA), nodeCoordinates, bag.PopTetromino(), false));
-						}
-						else if (registry.all_of<Components::QueueNode>(node.GetSource()))
-						{
-							auto& sourceNode = registry.get<Components::QueueNode>(node.GetSource());
-							
-							node.SetContent(sourceNode.GetContent());
-							RelocateTetromino(registry, nodeCoordinates, node.GetContent());
-							sourceNode.SetContent(entt::null);
+		} while (atLeastOneNodeIsEmpty);
 
-							//auto& moveable = registry.get<Components::Moveable>(node.GetContent());
-							//moveable.SetCurrentCoordinate(nodeCoordinates);
-							//moveable.SetDesiredCoordinate(nodeCoordinates);
-						}
-					}
+		if (currentFrameTime >= lastLockdownTime + generationTimeDelay && registry.view<Components::Controllable>().size() == 0)
+		{
+			auto nodeView = registry.view<Components::QueueNode>();
+			for (auto entity : nodeView)
+			{
+				auto& node = registry.get<Components::QueueNode>(entity);
+				if (node.GetDestination() == FindEntityByTag(registry, GetTagFromContainerType(containerType_t::MATRIX)))
+				{
+					PopFromQueueIntoMatrix(registry, currentFrameTime, entity);
 				}
 			}
 		}
