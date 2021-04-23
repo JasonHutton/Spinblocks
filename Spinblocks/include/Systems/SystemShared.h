cea40ad2b8e14c693c638b53a84696b2e2982800
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-23 03:23:44 -0700

    Remove StartFallSpeed as it's redundant.

--- Spinblocks/include/Systems/SystemShared.h
+++ Spinblocks/include/Systems/SystemShared.h
@@ -14,8 +14,7 @@
 	const int StartLevelGoal = 5;
 	inline int LevelGoal = 5;
 	inline int LinesClearedTotal = 0;
-	const double StartFallSpeed = 1.0; // Base fall speed, time it takes to move 1 line.
-	inline double FallSpeed = 1.0; // (0.8 - ((level - 1) * 0.007))^(level-1)
+	inline double FallSpeed = 1.0; // // Base fall speed, time it takes to move 1 line.... (0.8 - ((level - 1) * 0.007))^(level-1)
 	inline double lastFallUpdate = 0.0;
 	inline double lastLockdownTime = 0.0;
 	const double generationTimeDelay = 0.2; // Delay after last lockdown before a new generation occurs. (And a Tetromino is spawned into the play area matrix.)
