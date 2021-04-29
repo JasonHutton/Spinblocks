e827de39929267cbc249e9282121e07bdf3aab13
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-04-29 08:32:07 -0700

    ImGUI implementations for GLFW and OpenGL3. Some very temporary/rough implementations of ImGUI's initialization and supporting functions.

--- Spinblocks/src/Main.cpp
+++ Spinblocks/src/Main.cpp
@@ -48,6 +48,11 @@
 #include "GameState.h"
 
 #include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+
+// GL 3.0 + GLSL 130 // From ImGUI
+const char* glsl_version = "#version 130";
 
 using std::string;
 using std::cout;
@@ -1311,6 +1316,43 @@ int main()
 	glfwSwapInterval(1);
 	//glEnable(GL_DEPTH_TEST);
 
+	// IMGUI begin
+		// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// Load Fonts
+	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+	// - Read 'docs/FONTS.md' for more instructions and details.
+	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+	//io.Fonts->AddFontDefault();
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+	//IM_ASSERT(font != NULL);
+
+	// Our state
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	// IMGUI end
+
 	// Do one-time OpenGL things here.
 	Shader* shader = RetrieveShader("model", "./data/shaders/1.model_loading.vs", "./data/shaders/1.model_loading.fs");
 	
@@ -1358,6 +1400,18 @@ int main()
 		render(registry, GameTime::accumulator / GameTime::fixedDeltaTime);
 		postrender(registry, GameTime::accumulator / GameTime::fixedDeltaTime);
 
+		// IMGUI begin
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+		// IMGUI end
+
 		if (GameState::GetState() == gameState_t::GAME_OVER)
 		{
 			TeardownGame(registry);
@@ -1367,6 +1421,13 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents(); // Windows needs to do things with the window too!
 	}
+
+	// IMGUI begin
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	// IMGUI end
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
